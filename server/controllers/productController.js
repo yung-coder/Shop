0f594e76cc0b0207089636f5a28e43be02aa9f74
yung-coder <chandel.sanjeev5@gmail.com>
2023-02-12 23:35:48 +0530

    get single product

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -63,3 +63,22 @@ export const deletProduct = async (req, res, next) => {
     message: "Product deleted",
   });
 };
+
+// get product details
+
+export const productDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+
+  res.status(200).json({
+    success: true,
+    product
+  });
+};
--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -5,12 +5,17 @@ import {
   createProduct,
   updateProduct,
   deletProduct,
+  productDetails,
 } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
-router.route("/product/:id").put(updateProduct).delete(deletProduct);
+router
+  .route("/product/:id")
+  .put(updateProduct)
+  .delete(deletProduct)
+  .get(productDetails);
 
 export default router;

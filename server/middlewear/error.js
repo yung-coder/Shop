1fe78570241e8d6ee76aafe86cc27d252f9243a2
yung-coder <chandel.sanjeev5@gmail.com>
2023-02-18 19:10:21 +0530

    error handling fixing

--- server/middlewear/error.js
+++ server/middlewear/error.js
@@ -11,6 +11,21 @@ export const Customerror = (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  if (err.name === "jsonWebTokenError") {
+    const message = `json web token is invalid try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  if (err.name === "TokenExpiredError") {
+    const message = `Json web tokem expired`;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,

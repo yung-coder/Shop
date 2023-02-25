696f8484067d0b71e8292f1a0fe284be7e51963c
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-02-25 18:33:58 +0530

    card comp

--- frontend/src/components/Home/Products.jsx
+++ frontend/src/components/Home/Products.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ProductCard from "../Card/ProductCard";
 
 const Products = ({ products }) => {
-  return (
+  return (  
     <div
-      className=" h-screen bg-gradient-to-b from-gray-900 via-purple-900 to-violet-600 flex justify-center  flex-col space-y-11"
+      className=" h-screen overflow-auto  bg-gradient-to-b from-gray-900 via-purple-900 to-violet-600 flex justify-center  flex-col space-y-11"
       id="products"
     >
-      <div className="w-full h-full flex    space-x-5 flex-wrap  justify-center items-center p-5 space-y-4">
+      <div className="w-full h-full flex  space-x-5 flex-wrap  justify-center items-center p-5 space-y-4">
         <div className="w-full flex justify-center items-center">
-          <h1 className="text-white uppercase  underline underline-offset-8 text-4xl ">
+          <h1 className="text-white uppercase  underline underline-offset-8 text-2xl md:text-4xl">
             Trending Products
           </h1>
         </div>

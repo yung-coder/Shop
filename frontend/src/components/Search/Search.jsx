6bdf6ebc702c43189ba01910cb2920c81642f1d3
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-01 22:19:59 +0530

    input style

--- frontend/src/components/Search/Search.jsx
+++ frontend/src/components/Search/Search.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { FcSearch } from "react-icons/fc";
 
 const Search = () => {
   const [Keyword, setKeyword] = useState("");
@@ -17,14 +18,26 @@ const Search = () => {
   };
   return (
     <>
-      <form action="" onSubmit={fromHandler}>
-        <input
-          type="text"
-          value={Keyword}
-          onChange={(e) => setKeyword(e.target.value)}
-        />
-        <input type="submit" value="Search" />
-      </form>
+      <div
+        className="justify-center items-center h-screen overflow-auto bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-yellow-200 via-emerald-200 to-yellow-200 flex  flex-col space-y-11"
+        id="products"
+      >
+        <form
+          action=""
+          onSubmit={fromHandler}
+          className="flex space-x-5 w-[500px]"
+        >
+          <input
+            type="text"
+            value={Keyword}
+            onChange={(e) => setKeyword(e.target.value)}
+            class="bg-green-50  text-white placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-4 dark:bg-gray-700 dark:border-green-500"
+          />
+          <button type="submit" value="Search">
+            <FcSearch size={45} />
+          </button>
+        </form>
+      </div>
     </>
   );
 };

ab220a537ca4dc46d50e2c41f28eb6ce4dc8365c
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-05 13:42:37 +0530

    slider initial

--- frontend/src/components/Product/Product.jsx
+++ frontend/src/components/Product/Product.jsx
@@ -5,10 +5,13 @@ import { getProduct } from "../../actions/producaction";
 import ProductCard from "../Card/ProductCard";
 import Pagination from "react-js-pagination";
 import "./Product.css";
+import { Typography } from "@mui/material";
+import { Slider } from "@mui/material";
 
 const Product = () => {
   const dispatch = useDispatch();
   const [currentPage, setCurrentPage] = useState(1);
+  const [price, setPrice] = useState([0, 25000]);
   const { loading, error, products, productCount, resultPerPage } = useSelector(
     (state) => state.products
   );
@@ -17,6 +20,10 @@ const Product = () => {
 
   const setCurrentPageNo = (e) => {
     setCurrentPage(e);
+  };
+
+  const priceHandler = (event, newPrice) => {
+    setPrice(newPrice);
   };
 
   console.log(products);
@@ -39,6 +46,18 @@ const Product = () => {
             <div className="w-full h-full flex  space-x-5 flex-wrap  justify-center items-center p-5 space-y-4">
               {products &&
                 products.map((product) => <ProductCard product={product} />)}
+            </div>
+
+            <div>
+              <Typography>Price</Typography>
+              <Slider
+                value={price}
+                onChange={priceHandler}
+                valueLabelDisplay="auto"
+                aria-labelledby="range-slider"
+                min={0}
+                max={25000}
+              />
             </div>
 
             {resultPerPage < productCount && (
